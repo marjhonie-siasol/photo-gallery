526c0c07dc388eb99b358d69b4ffc11dba5aa3be
Marjhonie <157345383+marjhonie-siasol@users.noreply.github.com>
2024-06-26 07:01:35 +0800

    Update api.js

--- react-node-photo-gallery-master/client/src/api.js
+++ react-node-photo-gallery-master/client/src/api.js
@@ -2,7 +2,6 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export const getImages = async (nextCursor) => {
 	const params = new URLSearchParams();
-
 	
 	if (nextCursor) {
 		params.append('next_cursor', nextCursor);
